70f56de138005b3d8b4eed036d6af16cbc197c99
tasneem-khan-swe <tasneem.khandev@gmail.com>
2025-11-13 17:44:20 +0530

    test commit

--- frontend/src/routing/index.tsx
+++ frontend/src/routing/index.tsx
@@ -5,7 +5,7 @@ export const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={"Home"} />
+        <Route index element={"Home Page"} />
         <Route path="blog" element={"Blog"} />
         <Route path="about" element={"About"} />
         <Route path="contact" element={"Contact"} />
